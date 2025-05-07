0e7610634e9294d0a0db61e07e62acb33696bfea
Shilpa B <Shilpa2@MacBook-Pro-10.local>
2025-05-07 13:53:31 -0500

    Fixed errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import App2 from './App2.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
